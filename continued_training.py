b2e3e4efb19d0e162ad361b548f1713ec0091267
JunyoungCho07 <chojunyoung070523@gmail.com>
2025-07-27 16:48:38 +0900

    modified:   continued_training.py 	modified:   enviornment2.yml

--- continued_training.py
+++ continued_training.py
@@ -5,11 +5,11 @@ if __name__ == '__main__':
     multiprocessing.freeze_support()
 
     # 이전에 학습한 모델 가중치 불러오기 (best.pt 또는 last.pt)
-    model = YOLO('C:/Users/cho-j/OneDrive/바탕 화면/Ecoli_2025/runs/segment/exp_transfer_yolov11/weights/best.pt')
+    model = YOLO('C:/Users/cho-j/OneDrive/바탕 화면/Ecoli_YOLO11_det/runs/train/gb4_exp13/weights')
 
     # 추가 학습 (continue training)
     model.train(
-        data='C:/Users/cho-j/OneDrive/바탕 화면/Ecoli_2025/data.yaml',
+        data='C:/Users/cho-j/OneDrive/바탕 화면/Ecoli_YOLO11_det/data/data.yaml',
         epochs=20,              # 추가로 학습할 epoch 수 (기존 학습과 합쳐지지 않음!)
         batch=8,
         imgsz=256,
@@ -17,7 +17,7 @@ if __name__ == '__main__':
         device=0,
         project='C:/Users/cho-j/OneDrive/바탕 화면/Ecoli_2025/runs',
         name='exp_continued_v11',
-        task='segment',
+        task='detection',
         resume=False            # resume은 체크포인트 자동 이어붙일 때만 True로 (여기선 False)
     )
 
